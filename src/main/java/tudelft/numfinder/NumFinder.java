3af9ba241da8c2f23bb0dd05f07df0b748fc0003
Tibi <tiberiumarinescu2019@gmail.com>
2021-01-19 13:24:10 +0200

    Test1

--- src/main/java/tudelft/numfinder/NumFinder.java
+++ src/main/java/tudelft/numfinder/NumFinder.java
@@ -1,15 +1,17 @@
 package tudelft.numfinder;
 
+import org.jetbrains.annotations.NotNull;
+
 public class NumFinder {
     private int smallest = Integer.MAX_VALUE;
     private int largest = Integer.MIN_VALUE;
 
-    public void find(int[] nums) {
+    public void find(@NotNull int[] nums) {
         for(int n : nums) {
 
             if(n < smallest)
                 smallest = n;
-            else if (n > largest)
+            if (n > largest)
                 largest = n;
 
         }
